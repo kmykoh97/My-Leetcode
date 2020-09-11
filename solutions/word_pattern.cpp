8003d46a42f14356d915ef98589283cbba72b771
kmykoh97 <kmykoh97@gmail.com>
2020-09-11 11:53:15 +0800

    added solutions

--- solutions/word_pattern.cpp
+++ solutions/word_pattern.cpp
@@ -21,11 +21,34 @@
 // Notes:
 // You may assume pattern contains only lowercase letters, and str contains lowercase letters that may be separated by a single space.
 
-// solution: 
+// solution: map
 
 class Solution {
 public:
     bool wordPattern(string pattern, string str) {
-        map<>
+        unordered_map<char, string> char_map;
+        unordered_map<string, char> word_map;
+        
+        stringstream s(str);
+        string word;
+        int n = pattern.size(), i = 0;
+        
+        while (s >> word) {
+            if (i == n) return false;
+
+            char c = pattern[i];
+            
+            if (char_map.count(c) != word_map.count(word)) return false;
+            if (char_map.count(c)) {
+                if ((word_map[word] != c) || (char_map[c] != word)) return false;
+            } else {
+                char_map.insert({c, word});
+                word_map.insert({word, c});
+            }
+
+            i++;
+        }
+
+        return i == n;
     }
 };
