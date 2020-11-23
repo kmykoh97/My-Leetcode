fde16811cc36b74f92125eec1312ee633704b042
kmykoh97 <kmykoh97@gmail.com>
2020-11-24 00:11:53 +0800

    added solutions

---
+++ solutions/house_robber_3.cpp
@@ -0,0 +1,59 @@
+// The thief has found himself a new place for his thievery again. There is only one entrance to this area, called the "root." Besides the root, each house has one and only one parent house. After a tour, the smart thief realized that "all houses in this place forms a binary tree". It will automatically contact the police if two directly-linked houses were broken into on the same night.
+
+// Determine the maximum amount of money the thief can rob tonight without alerting the police.
+
+// Example 1:
+
+// Input: [3,2,3,null,3,null,1]
+
+//      3
+//     / \
+//    2   3
+//     \   \ 
+//      3   1
+
+// Output: 7 
+// Explanation: Maximum amount of money the thief can rob = 3 + 3 + 1 = 7.
+// Example 2:
+
+// Input: [3,4,5,1,3,null,1]
+
+//      3
+//     / \
+//    4   5
+//   / \   \ 
+//  1   3   1
+
+// Output: 9
+// Explanation: Maximum amount of money the thief can rob = 4 + 5 = 9.
+
+// solution: dp, dfs
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    map<TreeNode*, int> m;
+    
+    int rob(TreeNode* root) {
+        int temp = 0;
+
+        if (!root) return 0;
+        if (m.count(root)) return m[root];
+        if (root->left) temp += rob(root->left->left) + rob(root->left->right);
+        if (root->right) temp += rob(root->right->left) + rob(root->right->right);
+
+        m[root] = max(root->val+temp, rob(root->left)+rob(root->right));
+
+        return m[root];
+    }
+};
